a1bb53a3117dca5dfd488c43ec96209eb0c945c4
Zx-Akito <82266736+Zx-Akito@users.noreply.github.com>
2022-05-01 06:15:00 +0700

    Add files via upload
    upload new project

---
+++ login.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+        <meta name="generator" content="Hugo 0.88.1">
+        <title>SosialLabPHP</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link href="assets/styles/login.css" rel="stylesheet">
+
+        <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+            font-size: 3.5rem;
+            }
+        }
+        </style>
+
+    </head>
+
+    <body class="text-center">
+        
+    <form class="form-signin">
+        <h1 class="h3 mb-3 font-weight-normal">Login</h1>
+            <label for="inputEmail" class="sr-only">Email address</label>
+            <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        
+            <label for="inputPassword" class="sr-only">Password</label>
+            <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        
+            <div class="mt-3">
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                <hr>
+                <p class="text-muted">Don't have an account? <a href="#">Sign up</a></p>
+            </div>
+    </form>
+
+    </body>
+</html>
