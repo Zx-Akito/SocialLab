587b8a2a6c654888c9edd43877d2edc39ef12b8b
Zx-Akito <zxakito89@gmail.com>
2022-04-30 17:00:07 -0700

    add new form

--- register.php
+++ register.php
@@ -38,11 +38,18 @@
         
     <form class="form-signin">
         <h1 class="h3 mb-3 font-weight-normal">Register</h1>
+
+            <label for="inputUsername" class="sr-only">Full Name</label>
+            <input type="text" id="inputUsername" class="form-control mb-3" placeholder="Enter your name" required autofocus>
+
             <label for="inputEmail" class="sr-only">Email address</label>
             <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
         
             <label for="inputPassword" class="sr-only">Password</label>
             <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+
+            <label for="inputPasswordConfirmation" class="sr-only">Confirm Password</label>
+            <input type="password" id="inputPasswordConfirmation" class="form-control" placeholder="Confirm Password" required>
         
             <div class="mt-3">
                 <button class="btn btn-lg btn-primary btn-block" type="submit">Sign up</button>
